c9263b7d47534a68ea01a7b88aa4d682f70e3a89
juanky-cyber <geaalias@gmail.com>
2026-01-22 15:45:05 +0100

    Nivel 6 Añadido

---
+++ 2 trimestre/React/nivel6_react/my-react-app/src/App.tsx
@@ -0,0 +1,10 @@
+import Panel from './components/Panel'
+
+export default function App() {
+  return (
+    <main style={{ maxWidth: 520, margin: '40px auto', fontFamily: 'system-ui' }}>
+      <h1>Tarea 6 - Mini Reto</h1>
+      <Panel />
+    </main>
+  )
+}
